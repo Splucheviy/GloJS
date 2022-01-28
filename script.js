be1168563b0387f244f52c34d67e5fe6dfc88b29
John Voronov <Ivan_ksolo@mail.ru>
2022-01-28 18:12:21 +0300

    Выполнено задание первого урока

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+alert("Выполнение первого урока");
+console.log("Вывели в консоль сообщение с любым текстом");
