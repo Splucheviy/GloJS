49b5fa0b7eccf20ddf2fbb10a1fbcfcf0e19e5e4
John Voronov <Ivan_ksolo@mail.ru>
2022-01-28 22:17:10 +0300

    Выполненное задание 2-го урока

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-alert("Выполнение первого урока");
-console.log("Вывели в консоль сообщение с любым текстом");
+let title = "Урок 2";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 15000;
+console.log(typeof screenPrice);
+let rollback = 59;
+let fullPrice = 70000;
+let adaptive = true;
+
+// вывод типов данных в консоль
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+// длина строки screens
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+);
+screens = screens.toLowerCase();
+screens = screens.split(",");
+console.log(screens);
+let percentageOfRollback = fullPrice * (rollback / 100);
+console.log(percentageOfRollback);
