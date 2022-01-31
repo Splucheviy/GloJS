d03eb18257b3f7547e9ae8ed7d73730df4996be9
John Voronov <Ivan_ksolo@mail.ru>
2022-01-31 19:44:48 +0300

    Выполнено задание 3-го урока

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
-// let title = "Урок 3";
-// let screens = "Простые, Сложные, Интерактивные";
-// let screenPrice = 15000;
-// console.log(typeof screenPrice);
-// let rollback = 59;
-// let fullPrice = 70000;
-// let adaptive = true;
+let title = prompt("Как называется ваш проект?");
+console.log(title);
+let screens = prompt(
+  "Какие типы экранов нужно разрабатывать",
+  "Простые, Сложные, Интерактивные"
+);
+console.log(screens);
+let screenPrice = +prompt("Сколько будет стоить данная работа", "15000");
+console.log(screenPrice);
+let rollback = 59;
 
-// // вывод типов данных в консоль
-// console.log(typeof title);
-// console.log(typeof fullPrice);
-// console.log(typeof adaptive);
-// // длина строки screens
-// console.log(screens.length);
-// console.log(
-//   "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
-// );
-// console.log(
-//   "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
-// );
-// screens = screens.toLowerCase();
-// screens = screens.split(",");
-// console.log(screens);
-// let percentageOfRollback = fullPrice * (rollback / 100);
-// console.log(percentageOfRollback);
+let adaptive = prompt("Нужен ли адаптив на сайте?");
+if (adaptive == "Да") {
+  adaptive = true;
+} else {
+  adaptive = false;
+}
+console.log(adaptive);
 
-// Материал 3 урока
-let answer = confirm("Тебе есть 18 лет?");
-console.log(answer);
-let answer2 = prompt("Сколько тебе лет?");
-console.log(answer2); // ответ всегда приходит в виде строки
-// Приведение типов данных, методы Number(), String(), Boolean, ...
-//parseInt(),parseFloat()
-// Ветвления
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service1);
+let service1Price = +prompt("Сколько это будет стоить?");
+console.log(service1Price);
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service2);
+let service2Price = +prompt("Сколько это будет стоить?");
+console.log(service2Price);
+
+let fullPrice = screenPrice + service1Price + service2Price;
+console.log(fullPrice);
+let servicePercentPrice = fullPrice - rollback;
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  console.log("Предоставляем скидку 10%");
+} else if (fullPrice < 30000 && fullPrice > 15000) {
+  console.log("Предоставляем скидку 5%");
+} else if (fullPrice <= 15000 && fullPrice >= 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так :(");
+}
+// вывод типов данных в консоль
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+// длина строки screens
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+);
+screens = screens.toLowerCase();
+screens = screens.split(",");
+console.log(screens);
+let percentageOfRollback = fullPrice * (rollback / 100);
+console.log(percentageOfRollback);
