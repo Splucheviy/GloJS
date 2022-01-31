d76fac0b04cd423659cfd07c38078e5aacbeff26
John Voronov <Ivan_ksolo@mail.ru>
2022-01-31 19:07:23 +0300

    изучение материалов 3-го урока

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-let title = "Урок 2";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 15000;
-console.log(typeof screenPrice);
-let rollback = 59;
-let fullPrice = 70000;
-let adaptive = true;
+// let title = "Урок 3";
+// let screens = "Простые, Сложные, Интерактивные";
+// let screenPrice = 15000;
+// console.log(typeof screenPrice);
+// let rollback = 59;
+// let fullPrice = 70000;
+// let adaptive = true;
 
-// вывод типов данных в консоль
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-// длина строки screens
-console.log(screens.length);
-console.log(
-  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
-);
-console.log(
-  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
-);
-screens = screens.toLowerCase();
-screens = screens.split(",");
-console.log(screens);
-let percentageOfRollback = fullPrice * (rollback / 100);
-console.log(percentageOfRollback);
+// // вывод типов данных в консоль
+// console.log(typeof title);
+// console.log(typeof fullPrice);
+// console.log(typeof adaptive);
+// // длина строки screens
+// console.log(screens.length);
+// console.log(
+//   "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
+// );
+// console.log(
+//   "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+// );
+// screens = screens.toLowerCase();
+// screens = screens.split(",");
+// console.log(screens);
+// let percentageOfRollback = fullPrice * (rollback / 100);
+// console.log(percentageOfRollback);
+
+// Материал 3 урока
+let answer = confirm("Тебе есть 18 лет?");
+console.log(answer);
+let answer2 = prompt("Сколько тебе лет?");
+console.log(answer2); // ответ всегда приходит в виде строки
+// Приведение типов данных, методы Number(), String(), Boolean, ...
+//parseInt(),parseFloat()
+// Ветвления
